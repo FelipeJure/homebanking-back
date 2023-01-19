803225455c38abdd2799bba93ecd3285b3a386ec
FelipeJure <felipe.jure05@gmail.com>
2023-01-19 13:09:07 +0100

    add loan model

--- src/models/Loan.js
+++ src/models/Loan.js
@@ -1,20 +1,57 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
+    // prestamo
     sequelize.define('loan', {
-        // prestamo
-        type: {
-            type: DataTypes.ENUM(['currentAccount', 'savingAccount']),
+        amount: {
+            type: DataTypes.INTEGER,
             allowNull: false
         },
-        typeOfMoney: {
-            type: DataTypes.STRING,
-            defaultValue: 'euros'
+        // period hace referencia a la cantidad de meses que tiene el usuario para devolver el dinero
+        period: {
+            type: DataTypes.INTEGER,
+            allowNull: false
         },
-// ver si es compatible ya que donde dice null, deberia ir la longitud del numero
-        money: {
+        // interest es la tasa de interes que pone el banco (se debe poner en porcentaje sin el signo %)
+        interest: {
             type: DataTypes.FLOAT(null,2),
             defaultValue: 0
-        }
+        },
+        request: {
+            type: DataTypes.DATE
+        },
+        accepted: {
+            type: DataTypes.DATE
+        },
+        /* cada estado tiene su caracteristica:
+            under review: es cuando el usuario solicita un prestamo y debe ser revisado y aceptado por el banco,
+            accepted: cuando el banco acepta el prestamo,
+            in process: cuando esta en espera a ser transferido el dinero al usuario,
+            completed: cuando el usuario recibio el ingreso del prestamo,
+            cancelled: puede ser cuando el usuario paga todas las cuotas del prestamo, o cuando esta under review y se arrepiente del prestamo.
+        */
+        status: {
+            type: DataTypes.ENUM(['under review', 'accepted','in process', 'completed', 'cancelled'])
+        },
+        // collect es un array de fechas en las que debe pagar cada cuota
+        collect: {
+            type: DataTypes.ARRAY(DataTypes.DATE)
+        },
+        // totalDue es un valor virtual (no se guarda en la base de datos)
+        // donde se obtiene el total a pagar con la tasa de interes.
+        totalDue: {
+            type: DataTypes.VIRTUAL,
+            get() {
+                return this.amount * (this.interest/100 + 1);
+            },
+        },
+        // installments son las cuotas mensuales que debe pagar el usuario
+        // tambien es un valor virtual 
+        installments: {
+            type: DataTypes.VIRTUAL,
+            get() {
+                return this.totalDue / period;
+            },
+        },
     });
 };
